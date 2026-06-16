dfde92d00918414ca7a169f5e5270646eb5876ca
hcmbang1980-dev <hcmbang1980@gmail.com>
2026-06-16 19:06:03 +0900

    feat: add /login route with LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import BoardPage from './pages/BoardPage'
 import PostPage from './pages/PostPage'
 import WritePage from './pages/WritePage'
 import MyPage from './pages/MyPage'
+import LoginPage from './pages/LoginPage'
 import './index.css'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/board/:type" element={<BoardPage />} />
         <Route path="/notice" element={<BoardPage />} />
         <Route path="/event" element={<BoardPage />} />
