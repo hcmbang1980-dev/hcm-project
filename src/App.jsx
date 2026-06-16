56b4a2af3559322199998c16a4ad620d42a21dfc
hcmbang1980-dev <hcmbang1980@gmail.com>
2026-06-16 14:51:44 +0900

    init: add app routing

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+import BoardPage from './pages/BoardPage'
+import PostPage from './pages/PostPage'
+import WritePage from './pages/WritePage'
+import MyPage from './pages/MyPage'
+import './index.css'
+
+function App() {
+  return (
+    <AuthProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/board/:type" element={<BoardPage />} />
+        <Route path="/notice" element={<BoardPage />} />
+        <Route path="/event" element={<BoardPage />} />
+        <Route path="/intro" element={<BoardPage />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/write/:type" element={<WritePage />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </AuthProvider>
+  )
+}
+
+export default App
